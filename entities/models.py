63efcf88b0d092fbfe5da9e4fe4392c63107266f
41SergioF <carlossergio2401@outook.com>
2022-09-28 21:19:15 -0300

    Entidade Studant finalizada

--- entities/models.py
+++ entities/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth import get_user_model
 
+def user_directory_path(instance, filename):
+    """Trouxe direto da documentação, para simplificar."""
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    extension = filename.split(".")[-1]
+    return f"user_{instance.user.id}/profile_pic.{extension}"
+
 class Student(models.Model):
-    
+
     user = models.OneToOneField(
         get_user_model(),
         primary_key=True,
@@ -10,6 +17,14 @@ class Student(models.Model):
         related_name="perfil",
     )
 
-    def __str__(self) -> str:
-        return f"Perfil de {self.user.username}"  # pylint: disable=E1101
+    foto_perfil = models.ImageField(
+        "Foto de perfil",
+        blank=True,
+        upload_to=user_directory_path,
+        default="user_default/profile.jpg",
+    )
+    
+    create_date  = models.DateTimeField(default=timezone.now)
 
+    def __str__(self) -> str:
+        return f"Perfil de {self.user.username}"
--- entities/views.py
+++ entities/views.py
@@ -1,6 +1,16 @@
+from http.client import ImproperConnectionState
 from rest_framework import viewsets
 from .models import Student
-from .serializers import StudentSerializer
+from .serializers import (
+   StudentSerializer,
+   UserSerializer,
+)
+from django.contrib.auth import get_user_model
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    
 
 class StudentViewSet(viewsets.ModelViewSet):
    queryset = Student.objects.all()
